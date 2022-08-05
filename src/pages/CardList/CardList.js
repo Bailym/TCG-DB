a28ff6499bd70f024a22f7158e831d4d5df72f7f
Bailym <bm39@hotmail.co.uk>
2022-08-05 15:01:24 +0100

    convert card to functional component

--- src/pages/CardList/CardList.js
+++ src/pages/CardList/CardList.js
@@ -1,72 +1,37 @@
 import * as React from 'react';
 import CardComponent from '../../components/cardComponent';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import { Button, Typography, Box, Grid, Item } from '@mui/material';
+import { Grid } from '@mui/material';
 var axios = require('axios');
 
 
+function CardList() {
+    const [cards, setCards] = React.useState([]);
+    const [cardComponents, setCardComponents] = React.useState([]);
 
-class CardList extends React.Component {
-
-    state = {
-        cards: [],
-        cardComponents: []
-    }
-
-
-
-    componentDidMount = async () => {
+    //on component mount
+    React.useEffect(() => {
         //get the parameters from the url
         let params = new URLSearchParams(window.location.search);
         let set = params.get('set');
+        const setData = require("../../../backend/cache/" + set)
 
-        let newComponents = []  //hold the components generated
-        let apiData = []    //the raw api data object
-
-        //set headers for axios
-        var config = {
-            method: 'get',
-            url: `https://api.pokemontcg.io/v2/cards?q=set.id:${set}`,
-            headers: {
-                'X-Api-Key': process.env.REACT_APP_TCG_API_KEY
-            }
-        };
-
-        //make axios request for this card set
-        await axios(config)
-            .then(async function (response) {
-                apiData = response.data
-                //create a component for each card in apiData
-                for (let i = 0; i < apiData.data.length; i++) {
-                    //the individual card in the set
-                    let card = apiData.data[i]
-                    //create a component from each card
-                    newComponents.push(
-                        <CardComponent key={card.id} ApiID={card.id} userID={null} cardID={null} />
-                    )
-                }
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-
-        //set the state of the component
-        this.setState({
-            cardComponents: newComponents
-        })
-    }
+        console.log(setData.data)
 
 
-    render = () => {
-        return (
-            <Grid container spacing={2} alignItems={"center"}>
-                {this.state.cardComponents}
-            </Grid>
-        );
-    }
+
+        let newComponents = setData.data.map((card) => 
+            <CardComponent key={card.id} ApiID={card.id} />
+        ) 
+        console.log(newComponents)
+        setCardComponents(newComponents)
+    }, [])
+
+    return (
+        <Grid container spacing={2} alignItems={"center"}>
+            {cardComponents}
+        </Grid>
+    );
 }
+
 
 export default CardList
