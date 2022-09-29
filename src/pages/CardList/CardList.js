e1d9c88be6082158d90360d173709c8b085c49ac
Bailym <bm39@hotmail.co.uk>
2022-09-29 01:28:25 +0100

    fix params on cardlist

--- src/pages/CardList/CardList.js
+++ src/pages/CardList/CardList.js
@@ -1,22 +1,25 @@
 import * as React from 'react';
 import CardComponent from '../../components/cardComponent';
 import { Grid } from '@mui/material';
+import { useLocation } from 'react-router-dom'
 var axios = require('axios');
 
 
 function CardList() {
     const [cardComponents, setCardComponents] = React.useState([]);
+    const location = useLocation()
 
     //on component mount
     React.useEffect(() => {
         //get the parameters from the url
-        let params = new URLSearchParams(window.location.search);
-        let set = params.get('set');
+        const searchParams = new URLSearchParams(location.search)
+        let set = searchParams.get('set');
+
         const setData = require("../../../backend/cache/" + set)
 
-        let newComponents = setData.data.map((card) => 
+        let newComponents = setData.data.map((card) =>
             <CardComponent key={card.id} ApiID={card.id} SetID={set} />
-        ) 
+        )
         setCardComponents(newComponents)
     }, [])
 
