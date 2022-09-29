1ae72cd0978d8723607f44226957f678b2f3e4b5
Bailym <bm39@hotmail.co.uk>
2022-09-29 12:18:48 +0100

    rewrite home as a functional component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,77 +1,69 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { Container, Typography, Button, Grid } from "@mui/material";
-var axios = require('axios');
+import "./Home.css";
+const axios = require('axios')
 
-class Home extends React.Component {
+function Home() {
 
-  state = {
-    buttonComponents: []
-  }
+  const [buttonComponents, setButtonComponents] = useState([])
 
-  componentDidMount = async () => {
-    /* await axios.get('/api/checkuser')  //call the server endpoint
-      .then(async response => {
-        if (response.data === false) {   //if false redirect to login (you are not logged in.)
-          window.location.href = "/login";
-        }
-      })
-      .catch(function (error) {
-        console.log(error);
-      }) */
+  //set headers for axios
+  const config = {
+    method: 'get',
+    url: `https://api.pokemontcg.io/v2/sets`,
+    headers: {
+      'X-Api-Key': process.env.REACT_APP_TCG_API_KEY
+    }
+  };
 
-    let tempComponents = [];
-    let apiData = [];
+  useEffect(() => {
+    //gets the titles of the sets from TCG API and creates buttons for each set
+    async function getSetTitles() {
 
-    //set headers for axios
-    var config = {
-      method: 'get',
-      url: `https://api.pokemontcg.io/v2/sets`,
-      headers: {
-        'X-Api-Key': process.env.REACT_APP_TCG_API_KEY
-      }
-    };
+      let apiData = [];
+      let tempComponents = [];
 
-    //make axios request for this card set
-    await axios(config)
-      .then(async response => {
-        //get the api data
-        apiData = response.data
-        let currentSetID = null
-        let currentSetName = null
+      //make axios request for this card set
+      await axios(config)
+        .then(async response => {
+          //get the api data
+          apiData = response.data
+          let currentSetID = null
+          let currentSetName = null
 
-        //create a button for each set
-        for (let i = 0; i < apiData.data.length; i++) {
-          currentSetID = apiData.data[i].id
-          currentSetName = apiData.data[i].name
-          tempComponents.push(
-            <Button fullwidth key={currentSetID} href={process.env.PUBLIC_URL + `/#/cardlist?set=${currentSetID}`} style={{ fontSize: "2vw", textAlign:"center"}}>{currentSetName}</Button>
-          ) //create a button. 
-        }
+          //create a button for each set
+          for (let i = 0; i < apiData.data.length; i++) {
+            currentSetID = apiData.data[i].id
+            currentSetName = apiData.data[i].name
+            tempComponents.push(
+              <Button id="setButton" key={currentSetID} href={process.env.PUBLIC_URL + `/#/cardlist?set=${currentSetID}`} >{currentSetName}</Button>
+            ) //create a button. 
+          }
 
-        //update the state with the new components
-        this.setState({
-          buttonComponents: tempComponents
+          //update the state with the new components
+          setButtonComponents(tempComponents);
         })
+    }
 
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
+    //call the async function
+    getSetTitles();
+  }, [])
 
-  render = () => {
-    return (
-      <Container>
-        <Typography variant="h2" align="center" paddingBottom="5vh" >Home</Typography>
-        <Grid
-          container
-          justify={"center"}
-          direction={"column"}>
-          {this.state.buttonComponents}
-        </Grid>
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <Typography id="pageHeader" variant="h2">Home</Typography>
+      <Grid
+        id="buttonGrid"
+        container
+        justify={"center"}
+        direction={"column"}>
+        {buttonComponents}
+      </Grid>
+    </Container>
+  );
+
+
 }
+
 
 export default Home;
