3fbe78eaabe2abdb2b673eb5f2e393012767bc76
Bailym <bm39@hotmail.co.uk>
2022-09-29 12:54:55 +0100

    add title and back button to cardlist

--- src/pages/CardList/CardList.js
+++ src/pages/CardList/CardList.js
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import CardComponent from '../../components/cardComponent';
-import { Grid } from '@mui/material';
+import { Grid, IconButton, Container, Typography } from '@mui/material';
 import { useLocation } from 'react-router-dom'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 var axios = require('axios');
 
 
 function CardList() {
     const [cardComponents, setCardComponents] = React.useState([]);
     const location = useLocation()
+    const [title, setTitle] = React.useState(null)
 
     //on component mount
     React.useEffect(() => {
@@ -15,18 +17,33 @@ function CardList() {
         const searchParams = new URLSearchParams(location.search)
         let set = searchParams.get('set');
 
-        const setData = require("../../../backend/cache/" + set)
+        //get the data for this set from the cache
+        const cacheData = require("../../../backend/cache/" + set)
 
-        let newComponents = setData.data.map((card) =>
+        //create components from the cache data
+        let newComponents = cacheData.data.map((card) =>
             <CardComponent key={card.id} ApiID={card.id} SetID={set} />
         )
+
+        //update state
         setCardComponents(newComponents)
+        setTitle(cacheData.data[0].set.name);
     }, [])
 
+    function goHome() {
+        window.location.href = "/#/"
+    }
+
     return (
-        <Grid container spacing={2} alignItems={"center"}>
-            {cardComponents}
-        </Grid>
+        <Container>
+            <IconButton size="large" onClick={() => goHome()}>
+                <ArrowBackIcon />
+            </IconButton>
+            {title == null ? null : <Typography id="pageHeader" variant="h2">{title}</Typography>}
+            <Grid container spacing={2} alignItems={"center"}>
+                {cardComponents}
+            </Grid>
+        </Container>
     );
 }
 
