00e842b0f37a2a360afdfebf3e4ce9f82e19f9e2
Bailym <bm39@hotmail.co.uk>
2022-09-29 11:28:26 +0100

    remove navbar and related pages

--- src/index.js
+++ src/index.js
@@ -6,11 +6,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import WantList from './pages/WantList/WantList';
-import About from './pages/About/About.js';
-import Login from './pages/Login/Login';
 import CardList from "./pages/CardList/CardList";
-import BottomNav from "./components/bottomNav";
 import { Container } from '@mui/material';
 import { Box } from '@mui/material';
 
@@ -19,15 +15,11 @@ class Index extends React.Component {
   render = () => {
     return (
       <Box id="appContainer" style={{ backgroundColor: "#d0d3d9", maxHeight: "100vh", display: "flex", flexDirection: "column" }}>
-        <BottomNav />
         <Container id="contentContainer" maxWidth="md" style={{ margin: "auto", backgroundColor: "#fff", height: "100vh", overflowY: "scroll" }}>
           <Router >
             <Routes>
               <Route exact path="/" element={<Home />} />
-              <Route exact path="/wantlist" element={<WantList />} />
-              <Route exact path="/about" element={<About />} />
               <Route exact path="/cardlist" element={<CardList />} />
-              <Route exact path="/login" element={<Home />} />
             </Routes>
           </Router>
         </Container>
